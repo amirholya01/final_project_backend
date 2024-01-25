ebbf269de527d96fbda20b84250a28d86d275d1e
amirholya01 <amirholya01@gmail.com>
2024-01-25 12:26:41 +0100

    make auth schema with Joi

---
+++ app/controllers/user/auth/Auth.Controller.js
@@ -0,0 +1,8 @@
+const Controller = require("../../Controller");
+class UserAuthController extends Controller{
+    
+}
+
+module.exports = {
+    UserAuthController : new UserAuthController()
+}
--- app/models/users.js
+++ app/models/users.js
@@ -3,9 +3,11 @@ const mongoose = require("mongoose");
 const Schema = new mongoose.Schema({
     firstName: {type: String},
     lastName: {type: String},
-    username: {type: String, required: true, lowercase: true, unique: true},
+    username: {type: String,required: true, lowercase: true, unique: true},
     email: {type: String, required: true, lowercase: true, unique: true},
-    password: {type: String, required: true}
+    password: {type: String, required: true},
+    activationToken: {type: String, required: true},
+    activated: {type: Boolean, default: false}
 })
 
 module.exports = {
